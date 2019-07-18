46480b74c22d109248a59fc5593e8edf90e060f9
ilumii <listeven073@gmail.com>
2019-07-18 19:57:22 -0400

    added nav + button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("hover").addEventListener("mouseover", hoverMe);
 function hoverMe() {
     alert("Hey, I told you not to hover over me!");
     
+
 var count = 0;
 
 function button_one() {
